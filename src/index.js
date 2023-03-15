2049fff8431e1fd7a54ea136bf190054bead3fc8
tuanta <tuantran.kum@gmail.com>
2023-03-15 11:52:43 +0700

    bai 6-1, 6-2 paste project CU sang MOI

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
-ReactDom.render(
-    <h1>TUANTRAN - HELLO - REACT - WEBPACK</h1>,
+// import './index.css';
+import App from './components/App';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+import { createStore, applyMiddleware } from 'redux';
+import appReducers from './reducers/index';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+const store = createStore(
+    appReducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
     document.getElementById('root')
-)
+);
+// registerServiceWorker();

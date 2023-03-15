9aca7ab3b700b77231abcfccbe0af4ae27053e8e
tuanta <tuantran.kum@gmail.com>
2023-03-15 11:09:42 +0700

    bai 5-4 cau hinh

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        bundle: './src/index.js'
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/,
+                exclude: './node_modules/'
+            },
+            {
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ],
+                test: /\.css$/
+            }
+        ]
+    }
+}
